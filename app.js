eaa12955466111fa3b96fce29a868e8879251722
adewalecharles <shyprince1@gmail.com>
2020-12-13 05:10:15 +0100

    routing, helpers, rendering html pages

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
-const http = require("http");
+const path = require("path");
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
 
-const routes = require('./routes')
-const server = http.createServer(routes)
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
 
-server.listen(3000);
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use("/admin", adminRoutes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+});
+app.listen(3000);
