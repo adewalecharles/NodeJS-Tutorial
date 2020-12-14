9fd267b5209df579f52f929bf1e240b536b3b9c4
adewalecharles <shyprince1@gmail.com>
2020-12-14 05:26:49 +0100

    switched to ejs templating engine

--- routes/admin.js
+++ routes/admin.js
@@ -3,13 +3,22 @@ const express = require("express");
 const rootDir = require('../util/path');
 const router = express.Router();
 
+const products = [];
+
 router.get("/add-product", (req, res, next) => {
-   res.sendFile(path.join(rootDir, 'views', 'add-product.html'));;
+  res.render('add-product', {
+    pageTitle: 'Add Product', 
+    path:'/admin/add-product',
+    activeAddProduct: true,
+    formsCSS: true,
+    productCSS: true,
+  });
 });
 
 router.post("/add-product", (req, res, next) => {
-  console.log(req.body);
+  products.push({title: req.body.title});
   res.redirect("/");
 });
 
-module.exports = router;
+exports.routes = router;
+exports.products = products;
